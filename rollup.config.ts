675c9d598da4b156e46805ecf114545c70d7a3e8
Michele Bini <mikbin80@gmail.com>
2025-01-29 18:54:39 +0100

    project dependencies update

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,38 +1,45 @@
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
-import sourceMaps from 'rollup-plugin-sourcemaps'
-import camelCase from 'lodash.camelcase'
-import typescript from 'rollup-plugin-typescript2'
-import json from 'rollup-plugin-json'
+import { defineConfig } from "rollup";
+import typescript from "@rollup/plugin-typescript";
+import terser from "@rollup/plugin-terser";
+import del from "rollup-plugin-delete";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
 
-const pkg = require('./package.json')
+console.log("🌶 rollup current mode: ", process.env.BUILD);
 
-const libraryName = 'algorithmsts'
+const InjectPlugin =
+  process.env.BUILD === "production"
+    ? [
+        terser(),
+        nodeResolve({
+          extensions: [".js", ".jsx", ".ts", ".tsx"],
+          mainFields: ["browser", "module", "main"],
+        }),
+        typescript(),
+        del({ targets: "dist/*", verbose: true }),
+      ]
+    : [
+        nodeResolve({
+          extensions: [".js", ".jsx", ".ts", ".tsx"],
+          mainFields: ["browser", "module", "main"],
+        }),
+        typescript(),
+      ];
 
-export default {
-  input: `src/${libraryName}.ts`,
+export default defineConfig({
+  input: "src/algoritmsts.ts",
+  plugins: [...InjectPlugin],
+  external: ["node:url", "node:path"],
   output: [
-    { file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true },
-    { file: pkg.module, format: 'es', sourcemap: true },
+    // commonjs
+    {
+      file: "dist/algoritmsts.min.cjs", //packageJson.main,
+      format: "commonjs",
+    },
+    // es module
+    {
+      file: "dist/algoritmsts.esm.js", //packageJson.module,
+      format: "esm",
+      sourcemap: true,
+    },
   ],
-  // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-  external: [],
-  watch: {
-    include: 'src/**',
-  },
-  plugins: [
-    // Allow json resolution
-    json(),
-    // Compile TypeScript files
-    typescript({ useTsconfigDeclarationDir: true }),
-    // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
-    commonjs(),
-    // Allow node_modules resolution, so you can use 'external' to control
-    // which external modules to include in the bundle
-    // https://github.com/rollup/rollup-plugin-node-resolve#usage
-    resolve(),
-
-    // Resolve source maps to the original source
-    sourceMaps(),
-  ],
-}
+});
