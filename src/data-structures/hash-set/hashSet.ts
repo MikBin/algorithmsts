90d437bc22bc0a2d5ed8cf534b89ababdf81601d
Michele Bini <mikbin80@gmail.com>
2025-10-26 22:12:41 +0100

    feat(data-structures): add HashSet

---
+++ src/data-structures/hash-set/hashSet.ts
@@ -0,0 +1,13 @@
+import { HashMap, Hasher, Equality } from '../hash-map';
+
+export class HashSet<T> {
+  private map: HashMap<T, true>;
+  constructor(hasher?: Hasher<T>, equals?: Equality<T>) { this.map = new HashMap<T, true>(hasher as any, equals as any); }
+  get size(): number { return this.map.size; }
+  add(v: T): void { this.map.set(v, true); }
+  has(v: T): boolean { return this.map.has(v); }
+  delete(v: T): boolean { return this.map.delete(v); }
+  clear(): void { this.map = new HashMap<T, true>(); }
+  values(): T[] { return this.map.keys(); }
+  toArray(): T[] { return this.values(); }
+}
