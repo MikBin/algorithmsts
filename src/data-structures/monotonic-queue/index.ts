1afeb75bb01b59db95a07d6d520ea11fe31a5a81
Michele Bini <mikbin80@gmail.com>
2025-10-26 22:13:14 +0100

    feat(data-structures): add MonotonicQueue

---
+++ src/data-structures/monotonic-queue/index.ts
@@ -0,0 +1,1 @@
+export { MonotonicQueue } from './monotonicQueue';
---
+++ src/data-structures/monotonic-queue/monotonicQueue.ts
@@ -0,0 +1,9 @@
+export class MonotonicQueue<T> {
+  private data: T[] = [];
+  constructor(private compare: (a: T, b: T) => number) {}
+  push(x: T): void { while (this.data.length && this.compare(this.data[this.data.length-1], x) > 0) this.data.pop(); this.data.push(x); }
+  pop(x: T): void { if (this.data.length && this.compare(this.data[0], x) === 0) this.data.shift(); }
+  front(): T | undefined { return this.data[0]; }
+  size(): number { return this.data.length; }
+  toArray(): T[] { return [...this.data]; }
+}
